7f15ed91f506c99ed730e1575745cc314180f9f4
Andre Felix <andrefelix.br@gmail.com>
2021-10-30 14:33:18 +0100

    Versão inicial

---
+++ src/main/java/br/com/feltex/clienteapi/ClienteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.feltex.clienteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/feltex/clienteapi/ClienteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.feltex.clienteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
