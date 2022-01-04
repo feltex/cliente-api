4d424447ff91f21289a7a22ce771954a5bf83964
Andre Felix <andrefelix.br@gmail.com>
2022-01-04 17:46:03 +0000

    CRUD completo

--- src/main/java/br/com/feltex/clienteapi/ClienteApiApplication.java
+++ src/main/java/br/com/feltex/clienteapi/ClienteApiApplication.java
@@ -12,6 +12,8 @@ import java.util.Arrays;
 @SpringBootApplication
 public class ClienteApiApplication {
 
+    private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+
     public static void main(String[] args) {
         SpringApplication.run(ClienteApiApplication.class, args);
     }
@@ -20,12 +22,12 @@ public class ClienteApiApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://cliente-ui"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", ACCESS_CONTROL_ALLOW_ORIGIN, "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+                ACCESS_CONTROL_ALLOW_ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN, "Access-Control-Allow-Credentials"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
